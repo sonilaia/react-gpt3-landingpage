124a3785761f30f0dce4fce2471f2b9c21c8d352
sonilaia <sonylaia14@gmail.com>
2023-09-05 09:27:08 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+
+import {
+  Footer,
+  Blog,
+  Possibility,
+  Features,
+  Header,
+  WhatGPT3,
+} from "./containers";
+import "./App.css";
+
+import { CTA, Brand, Navbar } from "./components";
+
+const App = () => {
+  return (
+    <div className="App">
+      <div className="gradient__bg">
+        <Navbar />
+        <Header />
+      </div>
+      <Brand />
+      <WhatGPT3 />
+      <Features />
+      <Possibility />
+      <CTA />
+      <Blog />
+      <Footer />
+    </div>
+  );
+};
+
+export default App;
